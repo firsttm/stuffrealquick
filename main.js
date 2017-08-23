afce433efb4be710fb9a5f692c1f95dc52946bf4
Ishan <seriouslyflabbergasted@gmail.com>
2017-08-23 22:42:22 +0530

    ss and more stuff

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
-let filterInput = document.getElementById('filterInput');
+$(document).ready(function(){
+    $('.jumbo > div:gt(0)').hide();
 
-filterInput.addEventListener('keyup', filterNames);
-
-function filterNames() {
-    let filterValue = document.getElementById('filterInput').value.toUpperCase();
-
-    let ul = document.getElementById('names');
-
-    let li = ul.querySelectorAll('li.collection-item');
-
-    for(let i = 0; i < li.length; i++){
-        let links = li[i].getElementsByTagName('a')[0];
-
-        if(links.innerHTML.toUpperCase().indexOf(filterValue) > -1){
-            li[i].style.display = '';
-
-        } else {
-            li[i].style.display = 'none';
-        }
-    }
-}
+    setInterval(function(){
+        $('.jumbo > div:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('.jumbo');
+    }, 3000);
+});
 
